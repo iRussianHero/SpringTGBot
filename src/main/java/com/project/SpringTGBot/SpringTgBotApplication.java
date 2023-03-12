528f8e91300aa6ca939df885dda897a842b1834d
Roman Yakushev <boxboxbox@inbox.ru>
2023-03-12 17:06:22 +0300

    Initial commit

---
+++ src/main/java/com/project/SpringTGBot/SpringTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.project.SpringTGBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/SpringTGBot/SpringTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SpringTGBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
